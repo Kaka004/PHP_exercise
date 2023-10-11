966068934180024693d25c8d95001ec849bc463d
Kayo Vinícius <kayo.vinicius034@gmail.com>
2023-10-11 17:15:17 -0300

    Alistamento do Exército
    Se aliste ao exército brasileiro

---
+++ exercio1.php
@@ -0,0 +1,11 @@
+<?php
+$homem = true;
+$idade = 17;
+
+if ($homem && $idade >= 18) {
+    echo ("BEM-VINDO AO EXÉRCITO");
+} 
+elseif ($homem && $idade < 18) {
+    echo("Espere até atingir a idade do alistamento.");
+} 
+?>
